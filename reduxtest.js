aa5c69dac19348942b9f9c3ca1487bcfe03e538d
Vaibhav Shettar <vaibhavshettar5@gmail.com>
2019-05-31 14:06:08 +0530

    second commit

--- reduxtest.js
+++ reduxtest.js
@@ -12,10 +12,18 @@ const myReducer = (state = initialState, action) => { //creating reducers
     if(action.type === 'ADD'){
         newState.age += 1;
     }
+
+    if(action.type === 'SUBSTRACT'){
+        newState.age -= 1;
+    }
     return newState;
 }
 
 const store = createStore(myReducer); //creating store
 
-store.dispatch({type: 'ADD'})
-console.log(store.getState());
+console.log("intial state"+JSON.stringify(store.getState()));
+store.dispatch({type: 'ADD'}); //creating action(type is one action we can take val) and dispatch
+console.log("changed state"+JSON.stringify(store.getState()));
+JSON.stringify
+store.dispatch({type: 'SUBSTRACT'});
+console.log("changed state"+JSON.stringify(store.getState()));
